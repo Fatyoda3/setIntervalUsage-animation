07d062f8563c5dad77082520ff22b5d19f0c4fc0
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-29 18:37:05 +0530

    `just some changes`

--- main.js
+++ main.js
@@ -1,55 +1,64 @@
 const generateScreen = (size) => {
-  return Array.from({ length: size }, () => Array.from({ length: size }, () => '_'));
+  return Array.from({ length: size }, () => Array.from({ length: size }, () => ' '));
 };
 
-const draw = (screen) =>
-  console.log(screen.map(lines => lines.join('')).join('\n'));
-
+const draw = (screen) => {
+  console.log('-'.repeat(screen.length + 2));
+  console.log(screen.map(lines => '|' + lines.join('') + '|').join('\n'));
+  console.log('-'.repeat(screen.length + 2));
+};
 const clearScreen = (screen) => {
   console.clear();
-  for (let i = 0; i < screen.length; i++) {
-    for (let j = 0; j < screen[i].length; j++) {
-      screen[i][j] = '_';
-    }
-  }
+  for (const i in screen)
+    for (const j in screen[i])
+      screen[i][j] = ' ';
 };
 
-const plotter = (screen, x, y, symbol) => screen[x][y] = symbol;
+const plotter = (screen, x, y, symbol) => screen[Math.floor(x)][Math.floor(y)] = symbol;
 const updatePosition = ({ x, y, dx, dy }) => ({ x: x + dx, y: y + dy });
 
+const generateBalls = (count, dxRange, dyRange) => {
+  const balls = [];
+
+  for (let index = 0; index < count; index++) {
+    const ball = {
+      count: index,
+      x: 0, y: 3,
+      symbol: '*',
+      dx: Math.random() * dxRange,
+      dy: Math.random() * dyRange
+    };
+    balls.push(ball);
+  }
+  return balls;
+};
 const main = () => {
-  const screen = generateScreen(20);
-  const ball0 = { count: 0, x: 0, y: 3, symbol: '*', dx: 1, dy: 0.5 };
-  const ball1 = { count: 1, x: 5, y: 3, symbol: 'o', dx: 1, dy: 1.5 };
-  const ball2 = { count: 2, x: 1, y: 3, symbol: '*', dx: 0.5, dy: 0.5 };
-  const balls = [ball0, ball1, ball2];
+  const screen = generateScreen(100);
+  const balls = generateBalls(3, 1, 1);
 
   setInterval(() => {
     clearScreen(screen);
 
     balls.forEach((ball) => {
-      if (ball.x < 0) {
-        ball.x = 0;
+      if (ball.x < 1) {
+        ball.x = 1;
         ball.dx = -ball.dx;
       }
-      if (ball.y < 0) {
-        ball.y = 0;
+      if (ball.y < 1) {
+        ball.y = 1;
         ball.dy = -ball.dy;
       }
 
       if (ball.x > screen.length - 2) {
         ball.dx = -ball.dx;
-
-        if (ball.dx > 0) {
+        if (ball.dx > 0)
           ball.x = screen.length - 2;
-        }
-
       }
       if (ball.y > screen[0].length - 2) {
         ball.dy = -ball.dy;
-        if (ball.dx > 0) {
+        if (ball.dx > 0)
           ball.y = screen[0].length - 2;
-        }
+
       }
 
       const { x, y } = updatePosition(ball);
@@ -60,12 +69,12 @@ const main = () => {
       console.log(ball.dx, ball.dy);
       console.log(ball.x, ball.y);
 
-      plotter(screen, Math.floor(ball.x), Math.floor(ball.y), ball.symbol);
+      plotter(screen, (ball.x), (ball.y), ball.symbol);
 
     });
 
     draw(screen);
-  }, 200);
+  }, 30);
 
 };
 main();
