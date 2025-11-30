5a3258e65105650b8b930c8f8bafa814a0a15b1f
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-30 09:20:57 +0530

    `Just updated code for marquee`

--- marquee.js
+++ marquee.js
@@ -1,70 +1,67 @@
 const generateScreen = (size) => Array.from({ length: size },
   () => Array.from({ length: size }, () => '_'));
-
 const clearScreen = (screen) => {
   console.clear();
   for (const i in screen)
     for (const j in screen[i])
       screen[i][j] = ' ';
 };
-
-const plotHorizontal = (screen, texts, currentOffset) => {
-  texts.forEach((currentText, line) => {
-    for (let index = 0; index < currentText.length; index++) {
-      const plotPoint = (currentOffset + index);
-      if (plotPoint < screen[line].length && plotPoint >= 0) {
-        screen[line][plotPoint] = currentText[index];
-      }
-    }
-  });
-};
-
-const plotHorizontalRev = (screen, texts, currentOffset) => {
-  texts.forEach((currentText, line) => {
-    for (let index = currentText.length - 1; index > -1; index--) {
-      const plotPoint = (currentOffset + index);
-      if (plotPoint < screen[line].length && plotPoint >= 0) {
-        screen[line][plotPoint] = currentText[index];
-      }
-    }
-  });
-};
 const draw = (screen) => {
-  console.log('-'.repeat(screen.length + 2));
-  console.log(screen.map(lines => '|' + lines.join('') + '|').join('\n'));
-  console.log('-'.repeat(screen.length + 2));
+  const border = '-'.repeat(screen.length + 2);
+  const body = screen.map(lines => `|${lines.join('')}|`).join('\n');
+  console.log([border, body, border].join('\n'));
+};
+const plotHorizontal = (screen, { text, offset }, lineToDrawAt) => {
+
+  for (let index = 0; index < text.length; index++) {
+    const plotPoint = offset + index;
+
+    if (plotPoint < screen[lineToDrawAt].length && plotPoint >= 0) {
+      screen[lineToDrawAt][plotPoint] = text[index];
+    }
+  }
 };
 
 const main = () => {
 
-  const texts = ["HELLO", "WORLD", "I LIKE TURTLE", 'THIS IS COOL AS HELL'];
-  const screen = generateScreen(20);
+  const hello = {
+    text: "HELLO",
+    offset: -2,
+  };
+  const world = {
+    text: "WORLD",
+    offset: -4
+  };
 
-  texts.forEach((text) => {
+  const turtle = {
+    text: "I LIKE TURTLE",
+    offset: -5
+  };
+  const cool = {
+    text: "THIS IS COOL AS HELL",
+    offset: -4
+  };
 
-    // setInterval(() => {
-    //   // clearScreen(screen);
-    //   // if (offset > screen.length) {
-    //   //   offset = -text.length;
-    //   // }
-    //   // const truncated = ++offset;
-    //   // plotHorizontal(screen, texts, truncated);
-    // }, 300);
-    let offset = text.length;
+  const texts = [hello, world, turtle, cool];
 
-    setInterval(() => {
-      clearScreen(screen);
-      if (offset <= 0) {
-        offset = -(offset + text.length);
+  const screen = generateScreen(10);
+
+  setInterval(() => {
+    clearScreen(screen);
+
+    texts.forEach((data, index) => {
+      if (data.offset > screen.length - 1) {
+        data.offset = -data.text.length;
       }
 
-      const truncated = --offset;
-      // console.log(truncated);
+      plotHorizontal(screen, data, index + 2);
 
-      plotHorizontalRev(screen, texts, truncated);
-      draw(screen);
-    }, 400);
-  });
+      data.offset++;
+    });
+    draw(screen);
+
+  }, 300);
+
 
 };
 
