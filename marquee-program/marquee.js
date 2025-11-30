9498ce783581fdad33e1562a987433ef78b004fc
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-30 10:21:07 +0530

    `Just made some changes to structure `

--- marquee-program/marquee.js
+++ marquee-program/marquee.js
@@ -1,41 +1,40 @@
 import { generateScreen, clearScreen, draw } from "./helper_functions.js";
-import { STATIC_DATA } from "./texts.js";
+import { staticData } from "./texts.js";
 
 const plotHorizontalLine = (screen, { text, offset }, lineToDrawAt) => {
+  const drawableLine = screen[lineToDrawAt];
 
   for (let index = 0; index < text.length; index++) {
     const plotPoint = offset + index;
-
-    if (plotPoint < screen[lineToDrawAt].length && plotPoint >= 0) {
-      screen[lineToDrawAt][plotPoint] = text[index];
+    if (plotPoint < drawableLine.length && plotPoint >= 0) {
+      drawableLine[plotPoint] = text[index];
     }
   }
 };
+const drawHorizontal = (texts, screen, skipLine) => {
+  let separator = skipLine;
 
-const drawHorizontal = (texts, screen) => {
   texts.forEach((data, index) => {
     if (data.offset > screen[index].length - 1) {
       data.offset = -data.text.length;
     }
-
-    plotHorizontalLine(screen, data, index);
+    separator += skipLine;
+    plotHorizontalLine(screen, data, separator);
     data.offset += 1;
   });
 };
 const plotVerticalLine = (screen, { text, offset }, lineToDrawAt) => {
   for (let index = 0; index < text.length; index++) {
     const plotPoint = offset + index;
-    // console.log('screen at plotPoint', plotPoint, screen[plotPoint] === undefined);
 
     if (plotPoint < screen.length && plotPoint >= 0) {
       screen[plotPoint][lineToDrawAt] = text[index];
     }
   }
 };
-
 const drawVertical = (texts, screen, skipLine) => {
   let separator = skipLine;
-  texts.forEach((data, lineToDrawAt) => {
+  texts.forEach((data) => {
     if (data.offset > screen.length) {
       data.offset = -data.text.length;
     }
@@ -44,27 +43,20 @@ const drawVertical = (texts, screen, skipLine) => {
     data.offset += 1;
   });
 };
+
 const main = () => {
-  const screen = generateScreen(32, 20);
-  const horizontalText = STATIC_DATA();
-  const verticalText = STATIC_DATA();
-  // const hello = { text: 'name', offset: -1 };
+  const screen = generateScreen(64, 20);
+  const horizontalText = staticData();
+  const verticalText = staticData();
+
   setInterval(() => {
     clearScreen(screen);
-    // drawHorizontal(horizontalText, screen);
-    drawVertical(verticalText, screen ,3);
-    // if (hello.offset > screen.length - 1) {
-    //   hello.offset = -hello.text.length;
-    // }
-    // // console.log(hello.offset, 'offset value ');
 
-    // plotVerticalLine(screen, hello, 0);
-    // hello.offset += 1;
+    drawHorizontal(horizontalText, screen, 2);
+    drawVertical(verticalText, screen, 4);
 
     draw(screen);
   }, 200);
-
-
 };
 
 main();
