e7544ff3a3302e1dc2ed34bb0f895a4381c291ba
Fatyoda3 <shivangsingh654@gmail.com>
2025-11-30 10:11:09 +0530

    ` debug code still present for vertical draws`

--- marquee-program/marquee.js
+++ marquee-program/marquee.js
@@ -25,40 +25,41 @@ const drawHorizontal = (texts, screen) => {
 const plotVerticalLine = (screen, { text, offset }, lineToDrawAt) => {
   for (let index = 0; index < text.length; index++) {
     const plotPoint = offset + index;
-    console.log('screen at plotPoint', plotPoint, screen[plotPoint] === undefined);
+    // console.log('screen at plotPoint', plotPoint, screen[plotPoint] === undefined);
 
-    if (plotPoint < screen.length - 1 && plotPoint >= 0) {
+    if (plotPoint < screen.length && plotPoint >= 0) {
       screen[plotPoint][lineToDrawAt] = text[index];
     }
   }
 };
 
-const drawVertical = (texts, screen) => {
-  texts.forEach((data, index) => {
-    if (data.offset > screen[index].length - 1) {
+const drawVertical = (texts, screen, skipLine) => {
+  let separator = skipLine;
+  texts.forEach((data, lineToDrawAt) => {
+    if (data.offset > screen.length) {
       data.offset = -data.text.length;
     }
-
-    plotVerticalLine(screen, data, index);
+    separator += skipLine;
+    plotVerticalLine(screen, data, separator);
     data.offset += 1;
   });
 };
 const main = () => {
   const screen = generateScreen(32, 20);
-  // const horizontalText = STATIC_DATA();
+  const horizontalText = STATIC_DATA();
   const verticalText = STATIC_DATA();
-  const hello = { text: 'name', offset: -1 };
+  // const hello = { text: 'name', offset: -1 };
   setInterval(() => {
     clearScreen(screen);
     // drawHorizontal(horizontalText, screen);
+    drawVertical(verticalText, screen ,3);
+    // if (hello.offset > screen.length - 1) {
+    //   hello.offset = -hello.text.length;
+    // }
+    // // console.log(hello.offset, 'offset value ');
 
-    if (hello.offset > screen.length - 1) {
-      hello.offset = -hello.text.length;
-    }
-    // console.log(hello.offset, 'offset value ');
-
-    plotVerticalLine(screen, hello, 0);
-    hello.offset += 1;
+    // plotVerticalLine(screen, hello, 0);
+    // hello.offset += 1;
 
     draw(screen);
   }, 200);
